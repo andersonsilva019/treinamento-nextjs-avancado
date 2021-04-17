fe8cb3399b14a290c6b5894448f67702e027a67a
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-04-17 13:25:26 -0300

    :wrench: chore: custom tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/**/*.{js,ts,jsx,tsx,html}', 
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: {
+        DEFAULT: '#ffffff',
+      },
+      cyan: {
+        500: '#61dafb',
+      },
+      gray: {
+        100: '#e1e1e6',
+        300: '#a8a8b3',
+        900: '#121214'
+      },
+      yellow:{
+        500: '#eba417',
+      }
+    },
     extend: {},
   },
   variants: {
