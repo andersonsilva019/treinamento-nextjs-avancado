b513eeca6b863bb2667a01116c7147f50b0f197e
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-04-29 09:29:23 -0300

    :sparkles: feat(dashboard): listagem de usuarios responsivo

--- dashboard/src/pages/users/index.tsx
+++ dashboard/src/pages/users/index.tsx
@@ -11,7 +11,8 @@ import {
   Th,
   Thead,
   Tr,
-  Text
+  Text,
+  useBreakpointValue
 } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
@@ -19,6 +20,12 @@ import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UserList() {
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Box>
       <Header />
@@ -41,17 +48,16 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" w="8">
+                <Th px={["4", "4", "6"]} color="gray.300" w="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th w="8"></Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -60,18 +66,7 @@ export default function UserList() {
                     <Text fontSize="small" color="gray.300">andersonnsilva015@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>28 de Abril de 2021</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="small"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>28 de Abril de 2021</Td>}
               </Tr>
             </Tbody>
           </Table>
